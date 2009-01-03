f2f235c0408421946970246834fe6f3026e950fe
Contributions <contributions@curiosity-driven.org>
2009-01-02 21:38:53 -0800

    Display confirmation status

--- src/addresses.js
+++ src/addresses.js
@@ -32,10 +32,24 @@ function generateQR(address, amount) {
 }
 
 function getPaymentAddress() {
+  var keys = localStorage.getItem('keys');
+  if (keys) {
+    return JSON.parse(keys).publicAddress;
+  }
   var privateKey = secureRandom.randomBuffer(32);
-  var key = new CoinKey(privateKey); //compressed by default
+  // see https://en.bitcoin.it/wiki/List_of_address_prefixes
+  var testNet = {
+    public: 111,
+    private: 239
+  };
+  var key = new CoinKey(privateKey, testNet); //compressed by default
   var wif = key.privateWif;
-  return key.publicAddress;
+  var publicAddress = key.publicAddress;
+  localStorage.setItem('keys', JSON.stringify({
+    privateKey: wif,
+    publicAddress: publicAddress
+  }));
+  return publicAddress;
 }
 
 module.exports.generateQR = generateQR;
--- src/checkoutqr.js
+++ src/checkoutqr.js
@@ -1,22 +1,46 @@
 /** @jsx React.DOM */
 
 var addresses = require('./addresses');
+var helloblock = require('./helloblock');
 var React = require('react');
 
 var CheckoutQR = React.createClass({
+  getInitialState: function() {
+    return {
+      done: false,
+      status: 'Not confirmed'
+    };
+  },
+  componentDidMount: function() {
+    helloblock.connectAndListenForTx(this.props.address, function(err, data) {
+      console.log.apply(console, arguments);
+      if (err) {
+        this.setState({
+          done: true,
+          status: 'Failed: ' + err
+        });
+      } else {
+        this.setState({
+          done: true,
+          status: 'Confirmed: ' + err
+        });
+      }
+    }.bind(this));
+  },
   render: function() {
     return (
       <div>
         <div className="modal-body center">
           <p>Pay {this.props.totalPrice} BTC to {this.props.address}</p>
           <img src={addresses.generateQR(this.props.address, this.props.totalPrice)} />
+          <div>Status: {this.state.status}</div>
         </div>
         <div className="modal-footer">
           <button type="button" className="btn btn-default" onClick={this.props.onRequestHide}>
             <span className="glyphicon glyphicon-shopping-cart"></span> Cancel
           </button>
-          <button type="button" className="btn btn-success">
-            Pay <span className="glyphicon glyphicon-play"></span>
+          <button type="button" className="btn btn-success" disabled={!this.state.done} onClick={this.props.onRequestHide}>
+            Done <span className="glyphicon glyphicon-play"></span>
           </button>
         </div>
       </div>
