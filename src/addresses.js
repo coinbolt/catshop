aea144f812e691dfd817d2874e64241c0615a6e8
JP Richardson <jprichardson@gmail.com>
2014-10-03 04:47:58 -0500

    addresses: use coininfo module for testnet versions

--- src/addresses.js
+++ src/addresses.js
@@ -1,18 +1,16 @@
 var secureRandom = require('secure-random')
 var CoinKey = require('coinkey')
+var coinInfo = require('coininfo')
 
 function getPaymentAddress() {
   var keys = localStorage.getItem('keys')
   if (keys) {
     return JSON.parse(keys).publicAddress
   }
+  
   var privateKey = secureRandom.randomBuffer(32)
-  // see https://en.bitcoin.it/wiki/List_of_address_prefixes
-  var testNet = {
-    public: 111,
-    private: 239
-  };
-  var key = new CoinKey(privateKey, testNet) //compressed by default
+
+  var key = new CoinKey(privateKey, coinInfo('BTC-TEST')) //compressed by default
   var wif = key.privateWif
   var publicAddress = key.publicAddress
 
