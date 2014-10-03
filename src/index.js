49bd69ce258f16450b21f1554a19d5e0b1f24bb6
JP Richardson <jprichardson@gmail.com>
2014-10-03 07:59:16 -0500

    index: make cats cheaper

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 /** @jsx React.DOM */
 
 var NUM_CATS = 6
-var LOW_PRICE = 0.0001 //cheap cat :)
-var HIGH_PRICE = 0.1
+var LOW_PRICE = 0.001 //cheap cat :)
+var HIGH_PRICE = 0.05
 
 var App = require('./components/app')
 var cats = require('./cat-data').getCats(NUM_CATS, LOW_PRICE, HIGH_PRICE)
