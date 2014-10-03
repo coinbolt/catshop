11df0a458cbf5709707044feae7d587790f2b7f9
JP Richardson <jprichardson@gmail.com>
2014-10-03 04:46:14 -0500

    addresses: ASI ftw

--- src/addresses.js
+++ src/addresses.js
@@ -1,25 +1,29 @@
-var secureRandom = require('secure-random');
-var CoinKey = require('coinkey');
+var secureRandom = require('secure-random')
+var CoinKey = require('coinkey')
 
 function getPaymentAddress() {
-  var keys = localStorage.getItem('keys');
+  var keys = localStorage.getItem('keys')
   if (keys) {
-    return JSON.parse(keys).publicAddress;
+    return JSON.parse(keys).publicAddress
   }
-  var privateKey = secureRandom.randomBuffer(32);
+  var privateKey = secureRandom.randomBuffer(32)
   // see https://en.bitcoin.it/wiki/List_of_address_prefixes
   var testNet = {
     public: 111,
     private: 239
   };
-  var key = new CoinKey(privateKey, testNet); //compressed by default
-  var wif = key.privateWif;
-  var publicAddress = key.publicAddress;
+  var key = new CoinKey(privateKey, testNet) //compressed by default
+  var wif = key.privateWif
+  var publicAddress = key.publicAddress
+
   localStorage.setItem('keys', JSON.stringify({
     privateKey: wif,
-    publicAddress: publicAddress
-  }));
-  return publicAddress;
+    publicAddress: publicAddres
+  }))
+
+  return publicAddress
 }
 
-module.exports.getPaymentAddress = getPaymentAddress;
+module.exports = {
+  getPaymentAddress: getPaymentAddress
+}
