6a2a91c76843c2005cd29d4243c89e9dd9b5a248
JP Richardson <jprichardson@gmail.com>
2014-10-10 08:13:32 -0500

    config: configure protocol from URL (useful if opened from Coinbolt)

--- src/config.js
+++ src/config.js
@@ -3,16 +3,18 @@ var storage = require('@storage')
 var _config = {}
 
 //clickable URL to open client, this is only useful if opened from Coinbolt
+var _protocol
 Object.defineProperty(_config, 'protocol', {
   enumerable: true,
   get: function() {
-    var config = storage.get('config') || {}
-    return config.protocol ? config.protocol : 'bitcoin'
+    //var config = storage.get('config') || {}
+    return _protocol ? _protocol : 'bitcoin'
   },
   set: function(value) {
-    var config = storage.get('config') || {}
-    config.protocol = value
-    storage.set('config', config)
+    //var config = storage.get('config') || {}
+    //config.protocol = value
+    //storage.set('config', config)
+    _protocol = value
   }
 })
 
@@ -31,5 +33,31 @@ Object.defineProperty(_config, 'unit', {
     storage.set('config', config)
   }
 })
+
+function configureFromUrl() {
+  if (window.location.hash) {
+    var hashData = window.location.href.split('#')[1]
+    if (hashData.indexOf('config') !== 0) return //not a 'config' url
+    
+    hashData = hashData.replace('config', '')
+    hashData = hashData.slice(1) //should have '?'
+    
+    var data = JSON.parse(new Buffer(hashData, 'base64').toString('utf8'))
+    console.log('data from URL:')
+    console.dir(data)
+
+    //only protocol for now
+    if (data.config && data.config.protocol)
+      _config.protocol = data.config.protocol
+
+  }
+}
+
+try {
+  configureFromUrl()
+} catch (err) {
+  console.error(err)
+}
+
 
 module.exports = _config
