601899c525c7a379fe49682206216dc7d7b38c80
JP Richardson <jprichardson@gmail.com>
2014-10-14 08:43:40 -0500

    components/about-modal: updated text

--- src/components/about-modal.js
+++ src/components/about-modal.js
@@ -13,15 +13,16 @@ var AboutModal = React.createClass({
         <div className="modal-body">
           <p>
             The <a href="https://www.coinbolt.com/catshop">Coinbolt Cat Shop</a> enables you to experience buying things online
-            with bitcoin without actually having to own any real bitcoin.
+            with bitcoin without actually having to own any real bitcoin by using the Bitcoin test network.
           </p>
           <ol>
             <li>
               You need to get a wallet that supports the <a href="https://en.bitcoin.it/wiki/Testnet">Bitcoin testnet</a>.
+              <a href="https://play.google.com/store/apps/details?id=com.coinbolt.bitcoin"> Coinbolt</a> or any other testnet supported wallet would work.
             </li>
             <li>
-              Next you need to acquire testnet coins from either <a href="http://tpfaucet.appspot.com/">TP Testnet Faucet</a> or
-              <a href="http://faucet.xeno-genesis.com/"> Mojocoin Testnet Faucet</a>
+              If not using Coinbolt, you need to acquire testnet coins from either <a href="http://tpfaucet.appspot.com/">TP Testnet Faucet</a> or
+              <a href="http://faucet.xeno-genesis.com/"> Mojocoin Testnet Faucet</a>.
             </li>
             <li>
               Buy your fake cats on Coinbolt Catshop!
